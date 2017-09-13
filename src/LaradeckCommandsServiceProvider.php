3dca7f7496ec5c1ca5e34932bba627604f316c1f
Denis Sandal <den.sandal@gmail.com>
2017-09-13 19:51:23 +0700

    Init package

---
+++ src/LaradeckCommandsServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ngtfkx\Laradeck\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaradeckCommandsServiceProvider extends ServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $commands = [
+
+    ];
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands($this->commands);
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
